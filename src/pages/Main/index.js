3ca95795861dee359500618eba8fad565a1a49eb
David Luna <dbluna@STFCPGC033950-W>
2019-05-31 15:00:00 -0300

    Finalizando layout app nubank

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,33 +1,93 @@
 import React from 'react';
 
 import{Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation} from './styles';
+
+import { Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
+import Menu from '~/components/Menu';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { statement } from '@babel/template';
 
 export default function Main() {
+  let offset = 0;
+
+  const translateY = new Animated.Value(0);
+
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+
+        }
+      }
+    ],
+    { useNativeDriver: true },
+  )
+
+  function onHandlerStateChange(event) {
+    if(event.nativeEvent.oldState == State.ACTIVE) {
+      let opened = false;
+      const { translationY } = event.nativeEvent;
+      offset += translationY;
+      if(translationY >= 100) {
+        opened = true;
+      } else {
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset = 0;
+      }
+          Animated.timing(translateY, {
+            toValue: opened ? 380 : 0,
+            duration: 200, 
+            useNativeDriver: true,
+          }).start(() => {
+            offset = opened ? 380 : 0;
+            translateY.setOffset(offset);
+            translateY.setValue(0);
+          });
+
+    }
+  }
+
   return (
     <Container>
       <Header />
       <Content>
-        <Card>
-          <CardHeader>
-            <Icon name="attach-money" sizer={28} color="#665"/>
-            <Icon name="visibility-off" sizer={28} color="#665"/>
-          </CardHeader>
-          <CardContent>
-            <Title>Saldo disponivel</Title>
-            <Description>R$ 15.000,00</Description>
-          </CardContent>
-          <CardFooter>
-            <Annotation>
-              Transferência de R$ 500,00 recebida de Thiago
-            </Annotation>
-          </CardFooter>
-        </Card>
+        <Menu translateY={translateY} />
+
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChange}
+        >
+          <Card style={{transform: [{
+            translateY: translateY.interpolate({
+              inputRange: [-350, 0, 380],
+              outputRange: [-50, 0, 380],
+              extrapolate: 'clamp',
+            })
+          }]}}>
+            <CardHeader>
+              <Icon name="attach-money" size={28} color="#665"/>
+              <Icon name="visibility-off" size={28} color="#665"/>
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo disponivel</Title>
+              <Description>R$ 15.000,00</Description>
+            </CardContent>
+            <CardFooter>
+              <Annotation>
+                Transferência de R$ 500,00 recebida de Thiago
+              </Annotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
       </Content>
-      <Tabs/>
+      <Tabs translateY={translateY}/>
     </Container>
   )
 }
